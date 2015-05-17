0324ade8359ac9ac6b8e77a924a5efb1a6a43c2c
masa <masahiro.satomi@gmail.com>
2015-05-18 00:28:18 +0900

    Update main.php

--- app/config/main.php
+++ app/config/main.php
@@ -5,6 +5,7 @@ defined("LANG") || define("LANG", "ja_JP");
 
 defined("APPNAME") || define("APPNAME", "TOMOPRO2");
 
-defined("TOPURL") ||  define("TOPURL", "http://localhost/mvc3/");
+defined("TOPURL") ||  define("TOPURL", "http://localhost/mvc/");
 
 defined("DS") || define("DS", DIRECTORY_SEPARATOR);
+
