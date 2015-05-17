eaf1915f2fd17fb66ddde16366042392508027dd
masa <masahiro.satomi@gmail.com>
2015-05-18 00:27:05 +0900

    Update logout.php

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -3,12 +3,11 @@
 class LogoutController extends Controller {
 	
 	public function __construct(){
-		
 		$_SESSION = array();
-        if (isset($_COOKIE[session_name()])) {
-            setcookie(session_name(), '', time()-86400, '../mvc3/');
-        }
-        session_destroy();
-        header("Refresh:0; url=".TOPURL);
+		if (isset($_COOKIE[session_name()])) {
+			setcookie(session_name(), '', time()-86400, '../mvc/');
+		}
+		session_destroy();
+		header("Refresh:0; url=".TOPURL);
 	}
 }
